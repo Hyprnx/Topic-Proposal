772f5f0a08203598158f8908312447576f84a269
Hyprnx <toducanh2001@gmail.com>
2022-03-18 13:42:45 +0700

    update

--- data_manager.py
+++ data_manager.py
@@ -15,11 +15,36 @@ def system_status():
     return {'status': 'OK'}
 
 @app.route('/test')
-def test():
+def demo():
     entry = request.json
     return {'result': entry}
 
+@app.route('/add_employee', methods=['POST'])
+def add_employee():
+    entry = request.json
+    # Adding method goes here
+    return {'result': 'Successfully added employee to the database'}
 
+
+@app.route('/add_customer', methods=['POST'])
+def add_customer():
+    entry = request.json
+    # Adding method goes here
+    return {'result': 'Successfully added customer to the database'}
+
+
+@app.route('/add_good', methods=['POST'])
+def add_good():
+    entry = request.json
+    # Adding method goes here
+    return {'result': 'Successfully added good to the database'}
+
+
+@app.route('/get_good', methods=['GET'])
+def get_good():
+    entry = request.json
+    # Adding method goes here
+    return {'result': 'Successfully got good information from databases'}
 
 if __name__ == '__main__':
     app.run(port=8088)
