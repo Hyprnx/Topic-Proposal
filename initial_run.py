719fd30a923d64d28166714ff9cf6ea6988c34c3
Hyprnx <toducanh2001@gmail.com>
2022-06-02 19:34:52 +0700

    update customer, employee,... information

--- initial_run.py
+++ initial_run.py
@@ -1,15 +1,57 @@
 from pymongo import MongoClient
 import datetime
+import json
+from pprint import pprint
+from datetime import datetime
+import random
+import time
+import unicodedata
 
-host = 'localhost'
-port = 27017
+from mongo_db_connect import connect_to_database
+db = connect_to_database()
 
-client = MongoClient(host, port)
+employee = db.employee
+customer = db.customer
 
-blockchain_first_block = {
-            'index': 0,
-            'signer': 'To Duc Anh',
-            'timestamp': str(datetime.datetime.now()),
-            'data': 'Hello, this is the first block in the blockchain',
-            'previous_hash': '0'}
+# from unicode import unicode
+
+def random_date():
+    # random.seed(seed)
+    d = random.randint(1577811600, int(time.time()))
+    return datetime.fromtimestamp(d).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def normalize_name(name):
+    text = unicodedata.normalize('NFD', name).encode('ascii', 'ignore').decode("utf-8")
+    return str(text)
+
+def random_email(name):
+    return '_'.join(i for i in normalize_name(name).lower().split()) + '@bc.com'
+
+
+def get_employee(name):
+    respond = employee.find_one({'Name': name})
+    return respond['email'], respond['phone_number']
+
+def get_customer(name):
+    respond = customer.find_one({'Name': name})
+    return respond['Phone number']
+
+path = 'C:\git\Topic-Proposal\sample_data\Transactions.json'
+
+with open(path, 'r', encoding='utf8') as f:
+    data = json.loads(f.read())
+
+for i in data:
+    i['employee_email'], i['employee_phone'] = get_employee(i['employee_name'])
+    i['customer_phone'] = get_customer(i['customer_name'])
+
+
+
+write_path = 'C:\git\Topic-Proposal\sample_data\Transactions2.json'
+
+with open(write_path, 'w', encoding='utf8') as f_out:
+    f_out.write(json.dumps(data, ensure_ascii=False, indent=4))
+
+# pprint(data[0][''])
 
